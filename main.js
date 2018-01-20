b18901571bfa86c0cdc1738f7fd58503aeba4050
李照朋 <1392141777@qq.com>
2018-01-20 11:27:33 +0800

    main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+<template>
+  <div style="justify-content:center">
+    <text class="freestyle">Yo</text>
+  </div>
+</template>
+
+<style scoped>
+  .freestyle {
+    color: #41B883;
+    font-size: 233px;
+    text-align: center;
+  }
+</style>
